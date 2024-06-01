178a6a0df14a47b56fbc3c53d29e88388d0b6f21
DJlv <2743535685@qq.com>
2024-06-01 11:23:33 +0800

    img-componts

---
+++ docs/componts/imgUtils.vue
@@ -0,0 +1,27 @@
+<template>
+<div  class="demo-image__preview">
+  <el-image
+      style="width: 50px; height: 50px"
+      :src="url"
+      fit="fill"
+      :preview-src-list="srcList">
+  </el-image>
+</div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  props: ["urls"],
+  data() {
+    return {
+      url: 'http://localhost:5173/node-v/public/ueWord/'+ this.urls,
+      srcList: [
+        'http://localhost:5173/node-v/public/ueWord/'+ this.urls
+      ]
+    }
+  }
+}
+</script>
+<script setup>
+</script>
